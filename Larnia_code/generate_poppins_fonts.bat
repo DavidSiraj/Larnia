f5a67d2824becbb0b74467334f40e9f9daeb8854
Dávid Širaj <98828515+DavidSiraj@users.noreply.github.com>
2026-06-15 13:36:43 +0200

    Delete Larnia_code/generate_poppins_fonts.bat

--- Larnia_code/generate_poppins_fonts.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo.
-echo Larnia - generovanie Poppins fontov pre LVGL
-echo.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0tools\generate_poppins_lvgl.ps1"
-echo.
-pause
